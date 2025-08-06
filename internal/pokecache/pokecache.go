dd29c6531b9cdadf466c249c8aea9cbe32b4ba78
DarrenRickard <drickard94@gmail.com>
2025-08-07 02:08:31 +0900

    feat(package): added functionality to pokecache

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -12,9 +12,10 @@ type cacheEntry struct {
 
 type Cache struct {
 	entries			map[string]cacheEntry
+	interval		time.Duration
 }
 
-func NewCache(v []byte) cacheEntry {
+func newCacheEntry(v []byte) cacheEntry {
 	var c = cacheEntry {
 		createdAt: 	time.Now(),
 		val: 		v,	
@@ -22,11 +23,19 @@ func NewCache(v []byte) cacheEntry {
 	return c
 }
 
+func NewCache(i time.Duration) Cache {
+	var cache = Cache {
+		entries: make(map[string]cacheEntry),
+		interval: i,
+	}
+	return cache
+}
+
 func (c *Cache) Add(key string, val []byte ) error {
 	if c.entries == nil {
 		return fmt.Errorf("Error: cacheEntry map not initialized in Cache struct")
 	}
-	v := NewCache(val)
+	v := newCacheEntry(val)
 	c.entries[key] = v 
 	return nil
 }
@@ -37,4 +46,11 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 		return nil, false	
 	}
 	return entry.val, true
+}
+
+func (c *Cache) reapLoop() {
+	// every Cache.interval, call reapLoop()
+	// remove all cacheEntry created more than Cache.interval time ago
+	// if time.Now().Sub(newCacheEntry.createdAt) > 5 * time.Second {delete the newCacheEntry}
+
 }
